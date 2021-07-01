7896c9a3072d8c7de43c5b5cda9e715035cf6fb5
Muhammad Riham <mdrihamcth@gmail.com>
2021-07-01 20:49:41 +0600

    added done

---
+++ null-vs-undefined.js
@@ -0,0 +1,36 @@
+let pakhi;
+function add(num1, num2){
+    console.log(num1 + num2);
+    return 
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+const premik = {name:"smart dude", phone:458421};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 9]
+console.log(ages[11]);
+let name;
+console.log(name);
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return;
+}
+const result = add(13, 12);
+console.log(result);
+
+function add(num1, num2 =10){
+    console.log(num1, num2);
+}
+const result = add (10);
+console.log(result);
+
+const user = {name:"Smart", phone:018175, gf:"Khaleda"}
+console.log(user.id);
